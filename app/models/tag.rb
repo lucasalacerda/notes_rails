04d1d516dcd74fc169d544ddeb83e7374521a987
Lucas <lucasalvlacerda@gmail.com>
2018-09-11 01:39:02 -0300

    Create models Notes, Tags, UsersNote, TagNotes

---
+++ app/models/tag.rb
@@ -0,0 +1,8 @@
+class Tag < ApplicationRecord
+  belongs_to :user
+  
+  has_many :tags_notes
+  has_many :notes, through: :tags_notes
+   
+  validates_presence_of :title, :user
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create email: 'faker@user.com', password: '12345678'
+user2 = User.create email: 'faker2@user.com', password: '12345678'
+
+note1 = Note.create user: user1, title: 'Nota 1 de usando seeds', body: 'Rails é do caralhooo1' 
+note2 = Note.create user: user2, title: 'Nota 2 de usando seeds', body: 'Rails é do caralhooo2' 
+note3 = Note.create user: user1, title: 'Nota 3 de usando seeds', body: 'Rails é do caralhooo3' 
+
+tag1 = Tag.create title: "Tag1", user: user1
+tag2 = Tag.create title: "Tag2", user: user2
+
+userNote1 = UsersNote.create user: user1, note: note1
+userNote2 = UsersNote.create user: user2, note: note2
+
+tagNote1 = TagNote.create tag: tag1, note: note1
+tagNote2 = TagNote.create tag: tag2, note: note3
