e30b45ede74feea6c6888420e8cfa8615ae241d5
Lucas <lucasalvlacerda@gmail.com>
2018-09-12 01:12:12 -0300

    Finaliza backend da aplicacao; cria cruds para tags, notes e users; finaliza controllers

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,8 @@
 class Tag < ApplicationRecord
   belongs_to :user
   
-  has_many :tags_notes
-  has_many :notes, through: :tags_notes
+  has_many :tag_note
+  has_many :notes, through: :tag_note
    
   validates_presence_of :title, :user
 end
