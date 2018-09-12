9da7af214a15f512f6e66ef1c9dafe7dbccbe2a6
Lucas <lucasalvlacerda@gmail.com>
2018-09-12 01:33:50 -0300

    configura o mailcatcher da aplicacao

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     @note = current_user.notes.find params[:note_id]
     @user.guest_notes << @note
 
-    # NoteMailer.new_note_permission(@note, @user).deliver_now
+    NoteMailer.new_note_permission(@note, @user).deliver_now
     render json: {message: "Compartilhamento bem sucedido!"}, status: :ok
   end
 
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     @note = current_user.notes.find params[:note_id]
     @user.guest_notes.delete(@note.id)
 
-    # NoteMailer.permission_revoked(@note, @user).deliver_now
+    NoteMailer.permission_revoked(@note, @user).deliver_now
     render json: {message: "Permissão revogada com sucesso!"}, status: :ok
   end
 
